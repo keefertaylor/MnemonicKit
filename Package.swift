681c5423e844f8e58e9f815f4e17db8f4dddfca0
Jacky <newbdez33@gmail.com>
2020-10-07 14:53:37 +0900

    update SPM

--- Package.swift
+++ Package.swift
@@ -8,25 +8,21 @@ let package = Package(
             name: "MnemonicKit",
             targets: ["MnemonicKit_iOS", "MnemonicKit_macOS"]),
     ],
+    dependencies: [
+        .package(
+            url: "https://github.com/krzyzanowskim/CryptoSwift", 
+            from: "1.3.2"
+        )
+    ],
     targets: [
         .target(
             name: "MnemonicKit_iOS",
-            dependencies: [
-                .package(
-                    url: "https://github.com/krzyzanowskim/CryptoSwift", 
-                    from: "1.3.2"
-                )
-            ],
+            dependencies: ["CryptoSwift"],
             path: "MnemonicKit",
             exclude: ["Info.plist"]),
         .target(
             name: "MnemonicKit_macOS",
-            dependencies: [
-                .package(
-                    url: "https://github.com/krzyzanowskim/CryptoSwift", 
-                    from: "1.3.2"
-                )
-            ],
+            dependencies: ["CryptoSwift"],
             path: "MnemonicKit",
             exclude: ["Info.plist"]),
         .testTarget(
